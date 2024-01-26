394b3d42e14841ea9044b0c0c995a831e59ff03f
nixpig <143995476+nixpig@users.noreply.github.com>
2024-01-26 05:15:00 +0000

    chore: a bit of tidying up

--- internal/app/handlers/index.go
+++ internal/app/handlers/index.go
@@ -1,9 +1,15 @@
 package handlers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/nixpig/bloggor/pkg/api"
+)
 
 func IndexHandler(c *fiber.Ctx) error {
+	a := api.WithContext(c)
+
 	return c.Render("index", fiber.Map{
+		"Api":   a,
 		"Title": "Hello, pig!",
 	})
 }
